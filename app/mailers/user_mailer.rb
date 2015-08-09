e966b00c7547685be43eb3b337a524dc556e60ab
OoogleBoogle <simattfield@gmail.com>
2015-08-09 16:50:57 -0700

    Added sign in mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ApplicationMailer
 	def contact_mail(user)
 		@user = user
+		attachments.inline['logo_text.svg'] = File.read('public/images/logo_text.svg')
 		mail(to: @user.email, subject: 'Thanks for you message!')
 	end
 
@@ -8,6 +9,13 @@ class UserMailer < ApplicationMailer
 		@name = name
 		@email = email
 		@body = body
+		attachments.inline['logo_text.svg'] = File.read('public/images/logo_text.svg')
 		mail(to: "simbobattfield@hotmail.com", from: @email, subject: "You have a new message!")
+	end
+
+	def welcome_email(user)
+		@user = user
+		attachments.inline['logo_text.svg'] = File.read('public/images/logo_text.svg')
+		mail(to: user.email, subject: "Welcome to Kevin Attfield Photography")
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,14 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   ratyrate_rater
+  after_create :send_welcome_email
 
   def full_name
   	[self.first_name, self.last_name].join(" ")
   end
+
+  private
+    def send_welcome_email
+      UserMailer.welcome_email(self).deliver_later
+    end
 end
