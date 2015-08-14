9af70c64191e71b3b3411b99cddcd749faab854f
OoogleBoogle <simattfield@gmail.com>
2015-08-14 14:26:14 -0700

    Trying to fix ratyrate security issue

--- Gemfile
+++ Gemfile
@@ -50,6 +50,7 @@ group :development do
   gem 'guard-spork'
   gem 'guard-livereload', '~> 2.4', require: false
   gem 'brakeman', :require => false
+  gem "letter_opener"
 end
 
 group :test do
--- app/controllers/rater_controller.rb
+++ app/controllers/rater_controller.rb
@@ -2,7 +2,7 @@ class RaterController < ApplicationController
 
   def create
     if user_signed_in?
-      obj = params[:klass].classify.constantize.find(params[:id])
+      obj = params[:klass].classify.safe_constantize.find(params[:id])
       obj.rate params[:score].to_f, current_user, params[:dimension]
       render :json => true
     else
